c4b8c1799353590b95b0b0a6a5afe12411454291
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-16 14:02:29 -0500

    Set up initial todo module

---
+++ todo.js
@@ -0,0 +1,14 @@
+function createTodo (
+    title,
+    description = '',
+    dueDate = '',
+    priority = 0,) {
+
+    const title = title;
+    const description = description;
+    const priority = priority;
+    const dueDate = dueDate;
+
+}
+
+export { createTodo };
