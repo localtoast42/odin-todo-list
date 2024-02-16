9b6968ae5d9d28540963865282ef38354bec65cd
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-16 14:15:12 -0500

    Set up initial project module

---
+++ src/project.js
@@ -0,0 +1,22 @@
+function createProject (title) {
+    const title = title;
+    const todosInProgress = [];
+    const todosCompleted = [];
+
+    const addTodo = (todoToAdd) => {
+        this.todosInProgress.push(todoToAdd);
+    }
+
+    const completeTodo = (todoToComplete) => {
+        this.todosInProgress.filter((todo) => todo !== todoToComplete);
+        this.todosCompleted.push(todoToComplete);
+    }
+
+    const removeTodo = (todoToRemove) => {
+        this.todosInProgress.filter((todo) => todo !== todoToRemove);
+        this.todosCompleted.filter((todo) => todo !== todoToRemove);
+    }
+
+}
+
+export { createProject };
