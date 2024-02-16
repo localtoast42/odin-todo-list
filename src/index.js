d52c01d44c4b914ed71b510d204e25c8fa6c865e
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-16 18:10:40 -0500

    Implement add project button functionality

--- src/index.js
+++ src/index.js
@@ -13,3 +13,11 @@ const projects = [];
 projects.push(defaultProject);
 
 projectUpdater.updateProjectList(projects);
+
+const addProjectButton = document.querySelector('#project-add');
+
+addProjectButton.addEventListener('click', () => {
+    let title = prompt('Enter project title:');
+    projects.push(createProject(title));
+    projectUpdater.updateProjectList(projects);
+});
